ff8ed53ba3c8ed55128c80fb11a8495f2583637d
Alex Maina <lexizgichbuoy@gmail.com>
2023-10-25 15:05:51 +0300

    get_request

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.status(201); // created
+  res.send("Hello Ninja!");
 });
 
 app.listen(port, () => {
