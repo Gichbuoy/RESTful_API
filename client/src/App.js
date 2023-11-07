c01a793e96ba85b9fe07ef60580c2e60731985e2
Mwadali <wellingtonmwadali@gmail.com>
2023-11-07 23:17:41 +0300

    add user form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import { Container } from "react-bootstrap";
 import UserList from "./components/UserList";
-import ReadUsers from './components/readUsers';
+import CreateUser from "./components/CreateUser";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {createGlobalStyle} from 'styled-components';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const BackgroundColor = createGlobalStyle `
 body{
@@ -14,10 +14,15 @@ body{
 export default () => {
   return (
     <>
-    <BackgroundColor/>
-    <Container fluid className="mt-5">
-      <UserList/>
-    </Container>
+      <BackgroundColor />
+      <Container fluid className="mt-5">
+        <BrowserRouter>
+          <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/create" element={<CreateUser />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </>
   );
 }
---
+++ client/src/components/CreateUser.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const CreateUser = () => {
+    const CreateUsersUrl = "http://localhost:4000/v1/user/all";
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+
+    //submit form 
+    const submitForm = async (event) => {
+        event.preventDefault();
+
+        const createUserPayload = {
+            name,
+            email,
+            city,
+            country,
+        };
+        try {
+            const res = await axios.post(`${CreateUsersUrl}`, payload);
+            //optional chaining operation
+            if (res.data?.status === "OK") {
+
+            } else {
+                //give error message
+            }
+
+        } catch (err) {
+            //something is wrong    
+        }
+    };
+    return (
+        <Container className="mb-5">
+            <Row className="justify-content-center">
+                <Col lg={6}>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Name"
+                                onChange={(fieldElement) => setName(fieldElement.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                onChange={(fieldElement) => setEmail(fieldElement.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="City"
+                                onChange={(fieldElement) => setCity(fieldElement.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Country</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Country"
+                                onChange={(fieldElement) =>
+                                    setCountry(fieldElement.target.value)
+                                }
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={submitForm}>
+                            Add User
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default CreateUser
