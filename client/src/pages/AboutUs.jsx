0ee07cd0efd0a6ba9e8f8e928426a38b3f3a1371
Mwadali <wellingtonmwadali@gmail.com>
2023-11-10 00:42:06 +0300

    about / contact

---
+++ client/src/pages/AboutUs.jsx
@@ -0,0 +1,19 @@
+import Layout from "../components/layout/Layout";
+import { Col, Row } from "react-bootstrap";
+
+const AboutUs = () => {
+  return (
+    <Layout>
+      <h3 class="text-center">About Us</h3>
+      <Row class="justify-content-center">
+        <div class="text-center">
+          This project was done by Alex Miricho [ Backend ] and Wellington Mwadali [ Frontend ] as <br/>the final project for the 
+          foundation period in ALX Software Engineering bootcamp.
+          <br />Happy Coding!
+        </div>
+      </Row>
+    </Layout>
+  );
+};
+
+export default AboutUs;
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+import Layout from "../components/layout/Layout"
+import { Col, Row } from "react-bootstrap";
+
+const Contact = () => {
+    const email1 = 'wellingtonmwadali@gmail.com';
+    const email2 = 'lexizgichbuoy@gmail.com';
+  return (
+    <Layout>
+      <h3 className="text-center">Contact Us</h3>
+      <Row className="justify-content-center">
+        <Col md={10}>
+          <p class="text-center">
+            Contact us at <span><a href={`mailto:${email1}`}>Wellington</a></span>/<span><a href={`mailto:${email2}`}>Alex</a></span>
+          </p>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default Contact;
