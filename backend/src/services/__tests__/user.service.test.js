a71df3885e915120235c61608e077edcbdcb3bb4
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-13 00:34:57 +0300

    add tests

--- backend/src/services/__tests__/user.service.test.js
+++ backend/src/services/__tests__/user.service.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import userService from '../user.service';
+import userService from '../user.service.js';
 
 let sampleUser;
 
@@ -11,6 +11,13 @@ test.beforeEach(() => {
         city: 'Nairobi',
         country: 'Kenya',
     };
+});
+
+test.afterEach(() => {
+    if (userService.getUser(2)) {
+        console.warn('Cleanup: user 2 is being removed.');
+        userService.removeUser(2);
+    }
 });
 
 test('must add a user', (t) => {
@@ -34,16 +41,17 @@ test('must retrieve a user', (t) => {
 
 
 test('must get All users', (t) => {
-    const expectedId = 1;
+    // create a second User
+    userService.addUser(sampleUser);
 
-    const user = userService.getAllUsers();
+    const users = userService.getAllUsers();
     
-    t.deepEqual(user[0], {id: expectedId, ...sampleUser});
+    t.deepEqual(users, [{id: 1, ...sampleUser}, {id: 2, ...sampleUser}]);
 });
 
 
 test('must update a user', (t) => {
-    const expectedId = 1;
+    const userId = 1;
 
     const updatedDetails = {
         name: 'Alexx Gich',
@@ -52,20 +60,20 @@ test('must update a user', (t) => {
         country: 'Kenya',
     };
 
-    const user = userService.updateUser(1, updatedDetails);
+    const user = userService.updateUser(userId, updatedDetails);
     
-    t.is(user.id, expectedId);
-    t.deepEqual(user, {id: expectedId, ...updatedDetails});
+    t.is(user.id, userId);
+    t.deepEqual(user, {id: userId, ...updatedDetails});
 });
 
 test('must delete a user', (t) => {
     const userId = 1;
-    
+
     const expected = userService.removeUser(userId);
     
     t.is(expected, undefined);
 
     // Trying to retrive a removed user should return undefined
-    const user = userService.getUser(1);
+    const user = userService.getUser(userId);
     t.is(user, undefined);
 });
